c9356833d6c8615fbbdb9e9adb98f47f6a050461
miles <logan.duncan21@yahoo.com>
2022-05-10 14:35:12 -0500

    input groups updated for consistency

--- src/middleware/inputGroups.js
+++ src/middleware/inputGroups.js
@@ -1,6 +1,7 @@
 import { v4 as uuidv4 } from "uuid";
 import { useReducer, useState } from "react";
 import Amplify, { API, graphqlOperation, input } from "aws-amplify";
+import { createItem } from "../graphql/mutations";
 import awsExports from "../aws-exports";
 Amplify.configure(awsExports);
 
@@ -21,27 +22,42 @@ const newTransactionDefaultId = uuidv4();
 //     console.log(err, "error fetching Locations");
 //   }
 // }
+
 // indiv input groups
 // title - title of the input
 // value - the related value for a graphql request
 // select
+
 const addEmployee = {
   input1: {
+    title: "Employee Generated Id",
+    value: "id",
+  },
+  input2: {
     title: "Employee Full Name",
     value: "fullName",
   },
-  input2: {
+  input3: {
     title: "Employee Title",
     value: "title",
   },
-  input3: {
+  input4: {
     title: "Pay Rate",
     value: "pay",
   },
-  input4: {
+  input5: {
+    title: "Recently Helped",
+    value: "recentlyHelped",
+    givenValue: 0,
+  },
+  input6: {
     title: "Employement Location",
-    value: "employeeLocationId",
-    // ? options: Locations, do i need this
+    value: "workingAt",
+  },
+  input7: {
+    title: "Employement Transactions",
+    value: "transactions",
+    givenValue: null,
   },
 };
 
@@ -51,42 +67,61 @@ const addLocation = {
     value: "city",
   },
   input2: {
+    title: "City Name",
+    value: "city",
+  },
+  input3: {
     title: "Region",
     value: "region",
   },
-  input3: {
+  input4: {
     title: "Address",
     value: "address",
   },
-  input4: {
-    title: "Image",
+  input5: {
+    title: "Image, Placeholder currently",
     value: "image",
+  },
+  input6: {
+    title: "Employee List",
+    value: "employeeList",
+    givenValue: newCustomerDefaultId,
   },
 };
 
-const newCustomerInput = {
+const newCustomer = {
   input1: {
+    title: "Customer Generated ID",
+    value: "id",
+    givenValue: null,
+  },
+  input2: {
     title: "Customer Full Name",
     value: "fullName",
   },
-  input2: {
+  input3: {
     title: "Pester Customer about membership perks",
     value: "membership",
     boolean: true,
   },
-  input3: {
+  input4: {
     title: "Outstanding Payments",
     value: "outstandingPayments",
+    boolean: true,
   },
-  input4: {
+  input5: {
     title: "Generated Customer ID",
     value: "id",
-    genId: newCustomerDefaultId,
-    autoGen: true,
+    givenValue: newCustomerDefaultId,
+  },
+  input6: {
+    title: "Stored Items",
+    value: "storedItems",
+    givenValue: null,
   },
 };
 
-const lookupCustomerInput = {
+const lookupCustomer = {
   input1: {
     title: "Lookup with Customer Full Name",
     value: "fullName",
@@ -97,16 +132,16 @@ const lookupCustomerInput = {
   },
 };
 
+// adds an item and updates a customer
 const newItemExistingCustomer = {
-  input1: {
-    title: "Item Name being Stored",
-    value: "itemName",
-  },
   input2: {
     title: "Generated Item ID",
     value: "id",
-    genId: newItemDefaultId,
-    autoGen: true,
+    givenValue: newItemDefaultId,
+  },
+  input1: {
+    title: "Item Name being Stored",
+    value: "itemName",
   },
   input3: {
     title: "Quick Transfer?",
@@ -116,26 +151,24 @@ const newItemExistingCustomer = {
   input4: {
     title: "Customer ID",
     value: "customerStoredItemsId",
-    givenId: null,
+    givenValue: null,
   },
   input5: {
     title: "Transaction ID",
     value: "itemTransactionsId",
-    genId: newTransactionDefaultId,
-    autoGen: true,
+    givenValue: newTransactionDefaultId,
   },
 };
 
 const newItemAndCustomerInput = {
-  input1: {
-    title: "Item Name being Stored",
-    value: "itemName",
-  },
   input2: {
     title: "Generated Item ID",
     value: "id",
-    genId: newItemDefaultId,
-    autoGen: true,
+    givenValue: newItemDefaultId,
+  },
+  input1: {
+    title: "Item Name being Stored",
+    value: "itemName",
   },
   input3: {
     title: "Quick Transfer?",
@@ -145,47 +178,53 @@ const newItemAndCustomerInput = {
   input4: {
     title: "Customer ID",
     value: "customerStoredItemsId",
-    genId: newCustomerDefaultId,
-    autoGen: true,
+    givenValue: newCustomerDefaultId,
   },
   input5: {
     title: "Transaction ID",
     value: "itemTransactionsId",
-    genId: newTransactionDefaultId,
-    autoGen: true,
+    givenValue: newTransactionDefaultId,
   },
 };
 
-const newTransactionInput = {
+const newTransaction = {
   input1: {
     title: "Generated Transaction ID",
     value: "id",
-    genId: newTransactionDefaultId,
-    autoGen: true,
+    givenValue: newTransactionDefaultId,
   },
   input2: {
     title: "Customer ID",
     value: "customerAllTransactionsId",
-    givenId: null,
+    givenValue: null,
   },
   input3: {
-    title: "Item ID of Transaction",
-    value: "transactionsItemIDid",
-    genId: newItemDefaultId,
-    autoGen: true,
+    title: "Customer Name",
+    value: "customer Name",
+    givenValue: null,
   },
   input4: {
-    title: "Location Withdrawn from ID",
-    value: "transactionsLocationEndId",
+    title: "ItemId",
+    value: "itemID",
+    givenValue: newItemDefaultId,
   },
   input5: {
-    title: "Location Deposited ID",
-    value: "transactionsLocationStartId",
+    title: "Employee Id",
+    value: "employeeId",
+    givenValue: null,
+  },
+  input6: {
+    title: "Location Id of Transaction",
+    value: "locationId",
+    givenValue: null,
   },
   input7: {
-    title: "Placeholder, might not need isInStorage value",
-    value: "isInStorage",
-    boolean: true,
+    title: "Storage Value",
+    value: "isStored",
+  },
+  input8: {
+    title: "Date/Time of Transaction",
+    value: "dateTime",
   },
 };
 
@@ -200,19 +239,16 @@ export const inputGroupReducer = (state, action) => {
       return (state.currentInput = [addLocation]);
     case "addNewCustomerWithItem":
       return (state.currentInput = [
-        newCustomerInput,
+        newCustomer,
         newItemAndCustomerInput,
-        newTransactionInput,
+        newTransaction,
       ]);
     case "lookupCustomer":
-      return (state.currentInput = [lookupCustomerInput]);
+      return (state.currentInput = [lookupCustomer]);
     case "addExistingCustomerItem":
       newItemExistingCustomer.input4.givenId = action.payload.id;
-      newTransactionInput.input2.givenId = action.payload.id;
-      return (state.currentInput = [
-        newItemExistingCustomer,
-        newTransactionInput,
-      ]);
+      newTransaction.input2.givenId = action.payload.id;
+      return (state.currentInput = [newItemExistingCustomer, newTransaction]);
     default:
       throw new Error();
   }
